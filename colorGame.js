6ad672165b1e2c5ee6d30cfe70374e741615593a
TimmyJoseph <timitrilli@gmail.com>
2018-10-02 22:07:36 +0100

    Add update

--- colorGame.js
+++ colorGame.js
@@ -1,3 +1,33 @@
 var colors = [
     "rgb(255, 0, 0)",
+    "rgb(255, 225, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
 ]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("message");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+    //add initail colors to squares
+    squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function(){
+         //grab color of clicked square
+         var clickedColor = this.style.backgroundColor;
+         //compare color to pickedColor
+         if(clickedColor === pickedColor){
+             messageDisplay.textContent = "Correct!";
+         } else {
+             this.style.backgroundColor = "#232323";
+             messageDisplay.textContent = "Try again"
+         }
+    });
+}
