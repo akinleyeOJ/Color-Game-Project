c303b3523e97f4b39aa5f0f85b0868a8f73a9913
TimmyJoseph <timitrilli@gmail.com>
2018-10-05 16:30:00 +0100

    add js code to reset header colour

--- colorGame.js
+++ colorGame.js
@@ -14,7 +14,10 @@ resetButton.addEventListener("click", function(){
   //change colorDisplay to match picked color;
   colorDisplay.textContent = pickedColor;
   //change colors of squares
-  for(var i = 0; i < squares.l)
+  for(var i = 0; i < squares.length; i++){
+     squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
 });
 
 colorDisplay.textContent = pickedColor;
@@ -30,8 +33,9 @@ for(var i = 0; i < squares.length; i++){
          console.log(clickedColor, pickedColor);
          if(clickedColor === pickedColor){
              messageDisplay.textContent = "Correct!";
+             resetButton.textContent = "Play Again?"
              changeColors(clickedColor);
-             h1.style.backgroundColor = clickedColor
+             h1.style.backgroundColor = clickedColor;
          } else {
              this.style.backgroundColor = "#232323";
              messageDisplay.textContent = "Try Again"
